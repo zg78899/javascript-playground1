34abd7a264be50b0a0432ec48871eb0c2636bc44
zg78899 <zg788990@gmail.com>
2019-10-10 11:46:52 +0900

    javascript-playground

--- practice-1/1_8.js
+++ practice-1/1_8.js
@@ -1,8 +1,10 @@
 var sum = 0;
 
 for (var i = 1; i < 20; i++) {
-  if (!(i % 2 === 0 && i % 3 === 0)) {
+  if (!(i % 2 === 0) && !(i % 3 === 0)) {
     sum += i;
   }
 }
 console.log(sum);
+
+
--- text2.js
+++ text2.js
@@ -1,11 +1,12 @@
-var star = '';
 var line = 5;
+var star = '';
 
 for (var i = 0; i < line; i++) {
-  for (var j = 0; j < line;j++) {
-    
+  for (var j = 0; j <= i; j++) {
+    star += ' ';
+  }
+  for (var j = 0; j < 2 * (line - i) - 1; j++) {
     star += '*';
-
   }
   star += '\n';
 }
