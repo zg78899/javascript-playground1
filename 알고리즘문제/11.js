6a6ccae76be44620bd33e6bf0831e3a5a77d9354
zg78899 <zg788990@gmail.com>
2019-11-30 01:47:37 +0900

    practice -add

--- 알고리즘문제/11.js
+++ 알고리즘문제/11.js
@@ -1,8 +1,14 @@
+// 11.palindrome(팰린드롬 / 회문)은 왼쪽에서 오른쪽으로 읽은 다음,
+// 오른쪽부터 왼쪽으로 다시 읽어도 똑같은 형태와 의미를 유지하는 문장이나 단어를 지칭한다.
+// 인자로 전달한 문자열이 palindrome인지 검사하여 Boolean값을 반환하는 함수를 완성하라.
+// 단, 반드시 1자 이상의 문자열을 인자로 전달한다.
+
+
 function checkPalindrom(str) {
   console.log(typeof str);
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] = s[i] && s[i]= s[i + 2]) {
-      str.include()
+  // for (let i = 0; i < str.length; i++) {
+  //   if (str[i] = s[i] && s[i]= s[i + 2]) {
+  //     str.include()
 
     }
   }
---
+++ 연습1/date.js
@@ -0,0 +1,27 @@
+(function printNow(){
+const today=new Date();
+
+const dayName=['(일요일)','(월요일)','화요일','(수요일)','(목요일)','(금요일)','(토요일)'];
+
+const day=dayNames[today.getDay()];
+
+const year=day.getFullYear();
+const month=today.getMonth()+1;
+const date=today.getDate();
+let hour=today.getHours();
+let minutes=today.getMinutes();
+let second=today.getSeconds();
+const ampm=hour>=12?'PM':'AM';
+
+hour %12;
+hour=hour||12;
+
+
+minute=minute<10?'0'+minutes:minute;
+second=second<10?'0'+second:second;
+
+const now=`${year}년 ${month}월 ${date}일 ${day} ${hour}:${minute}:${second} ${ampm}`;
+
+console.log(now);
+setTimeout(printNow,1000);
+}());
