1a65ea3b54072dcb1f80bd49fd0334884642a6d5
zg78899 <zg788990@gmail.com>
2019-12-16 23:55:12 +0900

    added

---
+++ 알고리즘문제/나눠떨어지는 숫자배열.js
@@ -0,0 +1,18 @@
+function solution(arr, divisor) {
+  var answer = [];
+  for(var i=0;i<arr.length;i++){
+      if(arr[i] % divisor === 0) answer.push(arr[i]);
+  }
+  answer.sort(function(a,b){return a-b});
+  if(answer.length === 0) answer.push(-1);
+  return answer;
+}
+function solution(a,b){
+  let res=[];
+  for(let i=0;i<i.arr.length;i++){
+    if(a[i]%b===0)res.push(i);
+  }
+  res.sort(function(a,b){return a-b});
+  if(res.length === 0)res.push(-1);
+  return res;
+}
