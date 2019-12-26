22d31a9dd7ed4acc9ffa127841887c12dfa6fe5c
zg78899 <zg788990@gmail.com>
2019-12-26 13:54:02 +0900

    added

--- 알고리즘문제/최대 공약수,최소공배수문제.js
+++ 알고리즘문제/최대 공약수,최소공배수문제.js
@@ -1,19 +1,58 @@
-function abc(a, b) {
-  let answer = [];
-  let maxNum = Math.max(a, b);
-  let minNum = Math.min(a, b);
-  answer[0] = gcd(minNum, maxNum);
-  answer[1] = ccd(minNum, maxNum);
+// function abc(a, b) {
+//   let answer = [];
+//   let maxNum = Math.max(a, b);
+//   let minNum = Math.min(a, b);
+//   answer[0] = gcd(minNum, maxNum);
+//   answer[1] = ccd(minNum, maxNum);
+//   return answer;
+// }
+// //최대공약수
+// function gcd(minNum, maxNum) {
+//   return (minNum % maxNum) === 0 ? maxNum : gcd(minNum, minNum % maxNum);
 
-}
-//최대공약수
-function gcd(minNum, maxNum) {
-  return (minNum % maxNum) === 0 ? maxNum : gcd(minNum, minNum % maxNum);
 
+// }
+// //최소 공배수
+// function ccd(minNum, maxNum) {
+//   return (minNum * maxNum) / gcd(minNum, maxNum);
+// }
+// console.log(abc(2,5));
 
+// function greatCommonDivisor(a, b) {
+//   return b ? greatCommonDivisor(b, a % b) : Math.abs(a);
+// }
+// function leastCommonMultiple(a, b) {
+//   return (a * b) / greatCommonDivisor(a, b);
+// }
+// function gcd(a, b) {
+//   return [greatCommonDivisor(a, b), leastCommonMultiple(a, b)]
+// }
+// console.log(gcd(2,5));
+
+// function gcdlcm(a, b) {
+//   var gcd = calc_gcd(a, b);
+//   console.log(gcd);
+
+//   var lcm = (a * b) / gcd;
+
+//   return [gcd, lcm];
+// }
+
+// function calc_gcd(a, b) {
+//   if (b === 0) return a;
+//   return a > b ? calc_gcd(b, a % b) : calc_gcd(a, b % a);
+// }
+
+// // 아래는 테스트로 출력해 보기 위한 코드입니다.
+
+function gcdlcm(a, b) {
+  let gcd = calcGcd(a, b);
+  let lcm = (a * b) / gcd;
+  return [gcd, lcm];
 }
-//최소 공배수
-function ccd(minNum, maxNum) {
-  return (minNum * maxNum) / gcd(minNum, maxNum);
+function calcGcd(a, b) {
+  if (b === 0) return a;
+  return a > b ? calcGcd(b, a % b) : calcGcd(a, b % a);
 }
-console.log(abc(3,12));
+
+console.log(gcdlcm(3, 12));
