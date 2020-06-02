4fe10ca5cce08a554229df6a00e25bde9cfd43fe
kim jae hun <zg788990@gmail.com>
2020-06-03 01:14:24 +0900

    algorism

---
+++ 알고리즘문제/자료구조와 알고리즘/선형검색.js
@@ -0,0 +1,21 @@
+// 선형 검색을 통해 주어진 배열(array)에 주어진 값(target)이 요소로 존재하는지 확인하여 존재하는 경우 
+// 해당 인덱스를 반환하고 존재하지 않는 경우 -1을 반환하는 함수를 구현하라.
+//  단, 어떠한 빌트인 함수도 사용하지 않고 for 문을 사용하여 구현하여야 한다.
+
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if(array.indexOf(target) === i){
+      return i
+    }
+  }
+  return -1;
+  
+}
+
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(linearSearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 0)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 7)); // -1
