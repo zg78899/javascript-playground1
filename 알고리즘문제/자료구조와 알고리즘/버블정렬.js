c1cd64489cd4a8e9edcceae3b8f88c5f0d14faec
kim jae hun <zg788990@gmail.com>
2020-06-03 13:27:28 +0900

    algorism

---
+++ 알고리즘문제/자료구조와 알고리즘/버블정렬.js
@@ -0,0 +1,20 @@
+// 버블 정렬을 통해 주어진 배열(array)을 정렬하는 함수를 구현하라.
+// 단, 어떠한 빌트인 함수도 사용하지 않고 for 문을 사용하여 구현하여야 한다.
+
+function bubbleSort(array) {
+  let result =[...array];
+  for(let i =0;i<result.length -1;i++){
+    for(let j = 0;j<result.length -i;j++){
+      if(result[j] > result[j+1]){
+        let temp = result[j];
+        result[j] = result[j+1];
+        result[j+1] = temp;
+      }
+    }
+  }
+  return result;  
+}
+
+console.log(bubbleSort([2, 4, 5, 1, 3]));     // [1, 2, 3, 4, 5]
+// console.log(bubbleSort([5, 2, 1, 3, 4, 6]));  // [1, 2, 3, 4, 5, 6]
+// console.log(bubbleSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
---
+++ 알고리즘문제/자료구조와 알고리즘/선택정렬.js
@@ -0,0 +1,22 @@
+// 선택 정렬을 통해 주어진 배열(array)을 정렬하는 함수를 구현하라.
+// 단, 어떠한 빌트인 함수도 사용하지 않고 for 문을 사용하여 구현하여야 한다.
+
+function selectionSort(array) {
+  let result = [...array];
+  for (let i = 0; i < result.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < result.length; j++) {
+      if (result[j] < result[minIndex]) {
+        minIndex = j;
+      }
+    }
+    let temp = result[minIndex];
+    result[minIndex] = result[i];
+    result[i] = temp;
+  }
+  return result;
+}
+
+console.log(selectionSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
+console.log(selectionSort([2, 4, 5, 1, 3]));     // [1, 2, 3, 4, 5]
+console.log(selectionSort([5, 2, 1, 3, 4, 6]));  // [1, 2, 3, 4, 5, 6]
