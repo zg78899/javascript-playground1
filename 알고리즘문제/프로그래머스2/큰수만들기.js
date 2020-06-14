d4c652d20367822fb353ae99a24e6b59cb4ccdce
kim jae hun <zg788990@gmail.com>
2020-06-14 16:56:16 +0900

    algorism

---
+++ 알고리즘문제/프로그래머스2/큰수만들기.js
@@ -0,0 +1,65 @@
+ function solution(number,k){
+   var asnwer = '';
+   let res = [];
+   for(let i =0;i<number.length -k;i++){
+    let sortNum = [...number].sort((a,b)=>b-a);
+    
+    console.log(sortNum)
+  
+   }
+
+   return res;
+ }
+//  console.log(solution("1924",2));
+//  console.log(solution(1231234,2));
+ console.log(solution("4177252841",4));
+
+//  function solution(number,k){
+//    let index = 0;
+//    while(index < k){
+//      for(let i = 0; i<number.length;i++){
+//        if((number[i] < number[i+1]) ||
+//        (number[i] === '0' || number[i+1] === '0')){
+//          number = number.replace(number[i],'');
+//          break;
+
+//        }
+//      }
+//      index++;
+//    }
+//    return number;
+//  }
+function solution(number,k){
+  let stack =[];
+  let count = k;
+  for(let i =0;i<number.length;i++){
+    let now = number[i];
+    console.log(now);
+    while(stack[stack.length -1] < now){
+      console.log(stack);
+      stack.pop();
+      count--;
+      if(count === 0){
+        return stack.join('') + number.slice(i);
+      }
+    }
+    stack.push(now);
+  }
+  return stack.join('').slice(0,number.length -k);
+}
+
+function solution(number,k){
+  let b = [];
+  for(let i =0;i<number.length ;i++){
+    var c = number[i];
+    while(k > 0 && b[b.length - 1] < c && b.length >0){
+      b.pop();
+      k--;
+
+    }
+    b.push(c);
+  }
+  let answer =b.splice(b.length -k ,k).join('');
+  return answer;
+
+}
