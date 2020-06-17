9ebceae0458b5f1ecab590e0dd96e0d2c452dde0
kim jae hun <zg788990@gmail.com>
2020-06-18 00:44:54 +0900

    algorism

--- 알고리즘문제/프로그래머스2/구명보트.js
+++ 알고리즘문제/프로그래머스2/구명보트.js
@@ -5,6 +5,19 @@ function solution(people,limit){
   }
   return people.length - i;
 }
+
+function solution(people,limit){
+  people.sort((a,b)=>a -b);
+  for(let i = 0,j = people.length - 1;i<j;j--){
+    if(people[i] + people[j] <= limit)i++
+  }
+  return people.length -i;
+} 
+
+
+
+
+
 console.log(solution([70,50,80,50],100)); //return 3;
 console.log(solution([70,80,50],100)); //return 3;
 
@@ -27,12 +40,29 @@ function solution(people,limit){
   if(l == r) answer++;
   return answer;
 }
+
+
 //
 function solution(people,limit){
   people.sort((a,b)=> b-a);
   let count = 0;
   while(people.length){
     if(people[0] + people[people.length -1] <=limit){
+      people.shift();
+      people.pop();
+    }else{
+      people.shift();
+    }
+    count++;
+  }
+  return count;
+}
+
+function solution(people,limit){
+  people.sort((a,b)=>b-a);
+  let count = 0;
+  while(people.length){
+    if(people[0] +people[people.length - 1] <= limit){
       people.shift();
       people.pop();
     }else{
