b13c1bf3e82c334b3e643fa08cfcc552eaba4bc2
kim jae hun <zg788990@gmail.com>
2020-10-14 23:47:37 +0900

    revise

--- 연습1/예제/0-1.js
+++ 연습1/예제/0-1.js
@@ -1,15 +1,15 @@
-function solution() {
-  const array = [10, 20, 30, 40, 50, 60, 70, 80];
+function solution(array) {
   const answer = [];
   let min;
   let max;
   for (let i = 0; i < array.length; i++) {
     min = Math.min(...array);
     max = Math.max(...array)
-    const zeroOne = Math.round((array[i] - min) / (max - min));
-    answer.push(zeroOne);
+    const zeroOne = ((array[i] - min) / (max - min));
+    
+    answer.push(zeroOne.toFixed(3));
   }
-  return answer;
+  return answer.join(',');
 }
 
-console.log(solution())
+console.log(solution([10, 20, 30, 40, 50, 60, 70, 80]))
